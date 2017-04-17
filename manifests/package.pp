cdaef86cbc158dd3e87389fe67149b7d0f0ae64d
Dennis Dominguez <cheekyisgreat@gmail.com>
2017-04-17 15:38:40 -0600

    Update package.pp
    Added parameterization for source and provider.

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,7 +1,12 @@
 # Class: couchdb::package
 
-class couchdb::package {
+class couchdb::package (
+    $provider = undef
+    $source    = undef
+){
   package { 'couchdb':
     ensure  => installed,
+    source => $source,
+    provider => $provider,
   }
 }
